d9892875e4464177861ff98e3d646e64066be55a
Victor Hugo <victorhugosalviano25@gmail.com>
2024-01-19 17:11:00 -0300

    Implemented possible Moving Rook

--- src/application/UI.java
+++ src/application/UI.java
@@ -29,7 +29,7 @@ public class UI {
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     public static void clearScreen(){
-        System.out.print("0\33[H\033[2J");
+        System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,6 +30,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -40,6 +41,13 @@ public class ChessMatch {
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen pieces");
         }
+    }
+
+    private void validateTargetPosition(Position source, Position target){
+        if (!board.piece(source).possibleMove(target)){
+            throw new ChessException("The chosen piece can't move to target position");
+        }
+
     }
     private Piece makeMove(Position source, Position target){
         Piece p = board.removePiece(source);
