4dae638015a569fdd2f05c0d9ceae5a1d5fdd009
Victor Hugo <victorhugosalviano25@gmail.com>
2024-01-19 16:25:39 -0300

    Firsts Steps of Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,11 +2,28 @@ package application;
 
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(input);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(input);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
