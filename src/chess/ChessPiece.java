fba6327c9d9de32836ceacfc812241a5618fdad1
Victor Hugo <victorhugosalviano25@gmail.com>
2024-01-29 19:09:09 -0300

    Piece Move Count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,6 +6,7 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -17,6 +18,17 @@ public abstract class ChessPiece extends Piece {
     }
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
+    }
+
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+    public void decreaseMoveCount(){
+        moveCount--;
     }
     protected boolean isThereOpponentPiece(Position position){
         ChessPiece p = (ChessPiece) getBoard().piece(position);
